ecd39a580e5ecc097643741a40359fbac19ec88a
KowsalyaBaskar <kowsalyab2611@gmail.com>
2024-09-12 11:56:52 +0530

    project structure

---
+++ src/main/java/org/sample/sample.java
@@ -0,0 +1,10 @@
+package org.sample;
+
+public class sample {
+public static void main(String[] args) {
+	System.out.println("Test1");
+	System.out.println("Test2");
+	System.out.println("Test3");
+	System.out.println("Test4");
+}
+}
---
+++ src/test/java/org/login/Login.java
@@ -0,0 +1,7 @@
+package org.login;
+
+public class Login {
+public static void main(String[] args) {
+	System.out.println("Test");
+}
+}
