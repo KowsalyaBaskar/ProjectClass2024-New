684d0e83162ee61d04c234dfbceb692f59426ce5
Elakkiya0621 <Elakkiyamurugesan21@gmail.com>
2024-09-12 13:17:15 +0530

    Elakkiya's code-1

--- src/test/java/org/login/Login.java
+++ src/test/java/org/login/Login.java
@@ -6,5 +6,7 @@ public static void main(String[] args) {
 	System.out.println("Test 1");
 	System.out.println("Test 2");
 	System.out.println("Test 3");
+	System.out.println("Test 4");
+	System.out.println("Test 5");
 }
 }
