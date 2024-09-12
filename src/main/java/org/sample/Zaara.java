3ceccf235a91dde853f111203d3a631ae60e53a2
Elakkiya0621 <Elakkiyamurugesan21@gmail.com>
2024-09-12 12:34:50 +0530

    Zaara's code

--- src/main/java/org/sample/Zaara.java
+++ src/main/java/org/sample/Zaara.java
@@ -4,5 +4,8 @@ public class Zaara {
 
 	public static void main(String[] args) {
 		System.out.println("Zaara's code");
+		System.out.println("Test 4");
+		System.out.println("Test 5");
+		System.out.println("Test 6");
 	}
 }
